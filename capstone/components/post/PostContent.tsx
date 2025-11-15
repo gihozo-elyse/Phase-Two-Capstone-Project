944e5a7de33e3d846954547b62e012fe3eea31cf
gihozo-elyse <gihozoelyse@gmail.com>
2025-11-15 21:05:44 +0700

    implement markdown content renderer with GFM and code syntax highlighting

---
+++ capstone/components/post/PostContent.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkGfm from 'remark-gfm'
+
+interface PostContentProps {
+  content: string
+}
+
+export default function PostContent({ content }: PostContentProps) {
+  return (
+    <div className="markdown-content prose prose-lg max-w-none">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  )
+}
+
